21459c8dff83d5aa16ba8c7f2fc6c8e860c3997c
S-Roomi <sroomi2004@gmail.com>
2026-05-31 22:43:26 -0400

    Made unfinished mixing.py. Needs testing

--- mixing.py
+++ mixing.py
@@ -15,7 +15,7 @@ N_VALIDATION = 200
 
 CLEAN_DIR = Path("data/LibriSpeech/dev-clean")
 NOISE_DIR = Path("data/ESC-50-master/audio")
-
+OUTPUT_DIR = Path("data/mixed")
 
 def load_and_sample(path: Path, target_sr = SAMPLE_RATE):
     audio, sr = sf.read(path)
@@ -56,6 +56,29 @@ def main():
 
     print(f"Got {len(clean_files)} clean files and {len(noise_files)} noise files")
 
+    for split, n in [("train", N_TRAIN), ("validation", N_VALIDATION)]:
+        # out_clean = Path(f"{OUTPUT_DIR}/{split}/clean")
+        out_clean: Path = OUTPUT_DIR / split / "clean"
+        out_noisy: Path = OUTPUT_DIR / split / "noisy"
+        
+        out_clean.mkdir(parents=True, exist_ok=True)
+        out_noisy.mkdir(parents=True, exist_ok=True)
+
+        for i in range(n):
+            clean = load_and_sample(rand.choice(clean_files))
+            noise = load_and_sample(rand.choice(noise_files))
+            snr = rand.uniform(SNR_RANGE[0], SNR_RANGE[1])
+            
+            noisy_audio, clean_audio = mix(clean, noise, snr)
+
+            sf.write(out_noisy / f"{i:05d}.wav", noisy_audio, SAMPLE_RATE)
+            sf.write(out_clean / f"{i:05d}.wav", clean_audio, SAMPLE_RATE)
+
+            # For double checking
+            if i + 1 % 100 == 0:
+                print(f"{split}: {i+1}/{n}")
+
+    print("Finished")
 
 if __name__ == "__main__":
     main()
